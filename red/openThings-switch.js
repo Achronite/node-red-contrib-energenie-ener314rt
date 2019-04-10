318755cd7c4030ba6e45b9830cbab952bccd42cf
Achronite <10363609+Achronite@users.noreply.github.com>
2019-04-10 20:15:22 +0100

    Added monitoring to FSK switch node

--- red/openThings-switch.js
+++ red/openThings-switch.js
@@ -6,70 +6,92 @@
 **
 ** File: OpenThings-switch.js
 ** Purpose: Node-Red wrapper for call to switch node for ENER314-RT FSK/OpenThings (aka Monitor) devices
+**          Primarily intended for MiHome Adapter Plus - Control & Monitor
 **
 */
-var libradio = require( './libradio');
+var libradio = require('./libradio');
 
-module.exports = function(RED) {
+module.exports = function (RED) {
 
     function OpenThingsSwitchNode(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         var node = this;
 
         var device = RED.nodes.getNode(config.device);
         var board = RED.nodes.getNode(config.board);
+        var deviceId = Number(device.deviceId) || 0;
+        var productId = Number(device.productId) || 2;
 
-        node.on('input', function(msg) {
-            this.status({fill:"yellow",shape:"ring",text:"Sending"});
-            var deviceId = Number(device.deviceId) || 0;
-            var productId = Number(device.productId) || 2;
-            var xmits = 20;
+        // CHECK CONFIG
+        if (!deviceId || device == null || !board || board == null) {
+            this.status({ fill: "red", shape: "ring", text: "Not configured" });
+            return false;
+        } else {
+            node.on('input', function (msg) {
+                // Check all variables before we call the C routine to transmit, msg.payload overrides any defaults set in node
 
-            // Check all variables before we call the C routine to transmit, msg.payload overrides any defaults set in node
+                // Check OpenThings deviceId
+                if (deviceId == 0 || isNaN(deviceId)) {
+                    this.error("DeviceId err: " + deviceId + " (" + typeof (deviceId) + ")");
+                }
 
-            // Check OpenThings deviceId
-            if ( deviceId == 0 || isNaN(deviceId) ) {
-                this.error("DeviceId err: " + deviceId + " (" + typeof(deviceId) + ")");
-            }
+                // Check Switch State (default to off=0)
+                var switchState = 0;
+                if (typeof msg.payload == typeof true)
+                    switchState = msg.payload ? 1 : 0;
+                else if (typeof msg.payload.powerOn == typeof true)
+                    switchState = msg.payload.powerOn ? 1 : 0;
+                else if (msg.payload === "on" || msg.payload.powerOn === "on")
+                    switchState = 1;
 
-            // Check Switch State (default to off=0)
-            var switchState = 0;
-            if (typeof msg.payload == typeof true)
-                switchState = msg.payload ? 1 : 0;
-            else if (typeof msg.payload.powerOn == typeof true)
-                switchState = msg.payload.powerOn  ? 1 : 0;
-            else if (msg.payload === "on" || msg.payload.powerOn === "on")
-                switchState = 1;
-            
-             // Check xmit times (advanced), 26ms per payload transmission
-            if (Number(msg.payload.repeat))
-                xmits = Number(msg.payload.repeat);
+                // Set the node status in the GUI
+                switch (switchState) {
+                    case 1:
+                        node.status({ fill: "green", shape: "ring", text: "ON sent" });
+                        break;
+                    case 0:
+                        node.status({ fill: "red", shape: "ring", text: "OFF sent" });
+                        break;
+                }
 
-//            this.warn("Switching " +productId +":"+ deviceId + " switchState:" + switchState + " xmits:" + xmits+" device:"+device);
+                // Check xmit times (advanced), 26ms per payload transmission
+                var xmits = Number(msg.payload.repeat) || 20;
 
-            // Send payload
-            //var res = libradio.openThings_switch(productId, deviceId, switchState, xmits);
-            libradio.openThings_switch.async(productId, deviceId, switchState, xmits, function(err,res) {
-                // callback
-                if (err) {
-                    node.error("openThings_switch err: " + err);
-                    node.status({fill:"red",shape:"dot",text:"ERROR" });
-                } else {
-                    // Set the node status in the GUI
-                    switch (switchState) {
-                        case 1:
-                            node.status({fill:"green",shape:"dot",text:"ON"});
-                            break;
-                        case 0:            
-                            node.status({fill:"red",shape:"ring",text:"OFF"});
-                            break;
+                // Send payload (async)
+                libradio.openThings_switch.async(productId, deviceId, switchState, xmits, function (err, res) {
+                    // callback
+                    if (err) {
+                        node.error("openThings_switch err: " + err);
+                        node.status({ fill: "red", shape: "dot", text: "ERROR" });
+                    }
+                });
+
+                // dont send any payload for the input messages, as we are also a monitor node
+            });
+
+            board.events.on('monitor', function (OTmsg) {
+                if (OTmsg.deviceId == deviceId) {
+                    // received event for me
+
+                    // set node status for confirmed switch status
+                    if (OTmsg.SWITCH_STATE) {
+                        node.status({ fill: "green", shape: "dot", text: "ON" });
+                    } else {
+                        node.status({ fill: "red", shape: "dot", text: "OFF" });
                     }
 
-                    // return payload unchanged
-                    node.send(msg);
+                    // send on decoded OpenThings message as is
+                    node.send({ 'payload': OTmsg });
                 }
             });
-        });
+
+            board.events.on('error', function () { node.error("Board event error") });
+
+            this.on('close', function () {
+                // tidy up state
+            });
+        }
+
     }
-    RED.nodes.registerType("openThings-switch",OpenThingsSwitchNode);
+    RED.nodes.registerType("openThings-switch", OpenThingsSwitchNode);
 }
